699da41437b4f78c9dbf00df00188c04d51d3206
Jean Luc Emmanuel VERHANNEMAN <manmdyalw@gmail.com>
2021-10-20 17:35:46 +0200

    badge

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
+const moduleName = /^.+\/(.+)$/.exec(process.env.PWD)[1];
 gulp.task('compress', done => {
 
-    gulp.src('bystr-sort/*.js')
+    gulp.src(`./${moduleName}/*.js`)
     .pipe(uglify())
     .pipe(rename(function (path) {
         path.extname = `.min${path.extname}`;
       }))
-    .pipe(gulp.dest('./bystr-sort/dist'));
+    .pipe(gulp.dest(`./${moduleName}/dist`));
 
     done();
 });
